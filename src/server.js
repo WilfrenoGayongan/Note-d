d76f86db0036c07deee65f175f1d1d322e3e8920
Wilfeno Gayongan <WilfrenoGayongan@gmail.com>
2022-11-17 11:04:13 +0800

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path/posix";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+
+    res.render("home");
+
+})    
+
+app.get("/*", (req, res) => {
+    res.redirect("/")
+});
+
+
+app.listen(port,() => {
+    console.log("running..")
+});
